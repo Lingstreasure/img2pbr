6248411b064ae6a88f26132fb4d186d28232cce4
Lingstreasure <1486520814@qq.com>
2023-08-03 16:48:51 +0800

    Add empty MobileVit class(needs to be completed).

---
+++ src/models/components/mobile_vit.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+
+
+class MobileVit(nn.Module):
+    """"""
+    
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        pass
+        
+    def forward(self, x) -> torch.Tensor:
+        pass
