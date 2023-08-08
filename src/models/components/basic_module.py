af13ac1925d669c0ed40dee128c4d7a35c212237
Lingstreasure <1486520814@qq.com>
2023-08-08 17:06:27 +0800

    1.Add interpolation option for upsample. 2.Add num_group option for group normalization. 3.Add batch normalization option.

--- src/models/components/basic_module.py
+++ src/models/components/basic_module.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -7,6 +9,17 @@ from xformers.components.attention import LinformerAttention
 def Normalize(in_channels, num_groups=32):
     return nn.GroupNorm(num_groups=num_groups, num_channels=in_channels, eps=1e-6, affine=True)
 
+def conv_nd(dims, *args, **kwargs):
+    """
+    Create a 1D, 2D, or 3D convolution module.
+    """
+    if dims == 1:
+        return nn.Conv1d(*args, **kwargs)
+    elif dims == 2:
+        return nn.Conv2d(*args, **kwargs)
+    elif dims == 3:
+        return nn.Conv3d(*args, **kwargs)
+    raise ValueError(f"unsupported dimensions: {dims}")
 
 class ConvBlock(nn.Module):
     """A ConvBlock is as same architecture of ResdualBlock."""
@@ -15,12 +28,15 @@ class ConvBlock(nn.Module):
         self,
         in_channels: int,
         out_channels: int = 0,
+        use_batch_norm: bool = False,
+        num_groups: int = 16,
         *args, 
         **kwargs
     ) -> None:
         """Initialize a `ConvBlock`
         :param in_channels: The number of input channels.
         :param out_channels: The number of output channels. Default to `0`.
+        :param num_group: The number of normalization group. Default to `16`.
         """
         super().__init__(*args, **kwargs)
         
@@ -33,32 +49,42 @@ class ConvBlock(nn.Module):
                                kernel_size=3,
                                stride=1,
                                padding=1)
-        self.norm1 = Normalize(out_channels, num_groups=16)
+        if use_batch_norm:
+            self.norm1 = nn.BatchNorm2d(out_channels)  
+        else:
+            self.norm1 = Normalize(out_channels, num_groups=num_groups)
         
         self.conv2 = nn.Conv2d(out_channels,
                                out_channels,
                                kernel_size=3,
                                stride=1,
                                padding=1)
-        self.norm2 = Normalize(out_channels, num_groups=16)
+        if use_batch_norm:
+            self.norm2 = nn.BatchNorm2d(out_channels)
+        else:
+            self.norm2 = Normalize(out_channels, num_groups=num_groups)
+            
         
         self.skip = nn.Conv2d(in_channels,
                                out_channels,
                                kernel_size=1,
                                stride=1)
-        self.skip_norm = Normalize(out_channels, num_groups=16)
+        if use_batch_norm:
+            self.skip_norm = nn.BatchNorm2d(out_channels)
+        else:
+            self.skip_norm = Normalize(out_channels, num_groups=num_groups)
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         h = x
         h = self.conv1(h)
         h = self.norm1(h)
-        h = F.silu(h) 
+        h = F.silu(h)
         
         h = self.conv2(h)
         h = self.norm2(h)
         
         x = self.skip(x)
-        x = self.skip_norm(x)
+        # x = self.skip_norm(x)
         return h + x
     
     
@@ -69,6 +95,8 @@ class MLP(nn.Module):
         self, 
         in_channels: int,
         out_channels: int,
+        use_batch_norm: bool = False,
+        num_groups: int = 16, 
         dropout: float = 0.2, 
         *args, 
         **kwargs
@@ -76,7 +104,9 @@ class MLP(nn.Module):
         """Initialize a `MLP`
         :param in_channels: The number of input channels.
         :param out_channels: The number of output channels. Default to `3`.
-        :param dropout: The probability of Dropout layer
+        :param use_batch_norm: Whether to use Batch Normalization. Default to `False`.
+        :param num_group: The number of normalization group. Default to `16`.
+        :param dropout: The probability of Dropout layer.
         """
         super().__init__(*args, **kwargs)
         
@@ -85,7 +115,10 @@ class MLP(nn.Module):
                                  middle_channels,
                                  kernel_size=1,
                                  stride=1)
-        self.norm = Normalize(middle_channels, num_groups=16)
+        if use_batch_norm:
+            self.norm = nn.BatchNorm2d(out_channels)
+        else:
+            self.norm = Normalize(middle_channels, num_groups=num_groups)
         self.dropout = nn.Dropout(dropout)
         self.conv_out = nn.Conv2d(middle_channels,
                                   out_channels,
@@ -143,7 +176,8 @@ class LinearAttention(nn.Module):
     This implementation is from openaimodel.py in SD.
     """
     
-    def __init__(self, dim, heads=4, dim_head=32):
+    def __init__(
+        self, dim: int, heads: int = 4, dim_head: int = 32):
         super().__init__()
         self.heads = heads
         hidden_dim = dim_head * heads
@@ -159,6 +193,79 @@ class LinearAttention(nn.Module):
         out = torch.einsum('bhde,bhdn->bhen', context, q)
         out = rearrange(out, 'b heads c (h w) -> b (heads c) h w', heads=self.heads, h=h, w=w)
         return self.to_out(out)
+
+
+class Upsample(nn.Module):
+    """An upsampling layer with an optional convolution."""
+
+    def __init__(
+        self, 
+        in_channels: int, 
+        use_conv: bool, 
+        dims: int = 2, 
+        out_channels: Optional[int] = None, 
+        mode: str = "nearest",
+        padding=1
+    ) -> None:
+        """Initialize a Upsample module.
+        
+        :param in_channels: Channels in the inputs and outputs.
+        :param use_conv: A bool determining if a convolution is applied.
+        :param dims: Determines if the signal is 1D, 2D, or 3D. If 3D, then 
+                     upsampling occurs in the inner-two dimensions. Default to `2`.
+        :param out_channels: Channels in the outputs. Default to `None`.
+        :param mode: Mode in interpolation of upsampling. Default to `nearest`.
+        :param padding: padding of convolution layer. Default to `1`.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels or in_channels
+        self.use_conv = use_conv
+        self.mode = mode
+        self.dims = dims
+        if use_conv:
+            self.conv = conv_nd(dims, self.in_channels, self.out_channels, 3, padding=padding)
+
+    def forward(self, x):
+        assert x.shape[1] == self.channels
+        if self.dims == 3:
+            x = F.interpolate(
+                x, (x.shape[2], x.shape[3] * 2, x.shape[4] * 2), mode=self.mode
+            )
+        else:
+            x = F.interpolate(x, scale_factor=2, mode=self.mode)
+        if self.use_conv:
+            x = self.conv(x)
+        return x
+
+
+class Upsample2D(nn.Module):
+    """An upsampling layer with an 2D convolution."""
+
+    def __init__(
+        self, 
+        in_channels: int, 
+        out_channels: Optional[int] = None, 
+        mode: str = "nearest",
+        padding: int = 1
+    ) -> None:
+        """Initialize a Upsample2D module.
+        
+        :param in_channels: Channels in the inputs and outputs.
+        :param out_channels: Channels in the outputs. Default to `None`.
+        :param mode: Mode in interpolation of upsampling. Default to `nearest`.
+        :param padding: padding of convolution layer. Default to `1`.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels or in_channels
+        self.mode = mode
+        self.conv = nn.Conv2d(self.in_channels, self.out_channels, 3, padding=padding)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = F.interpolate(x, scale_factor=2, mode=self.mode)
+        x = self.conv(x)
+        return x
 
 
 class Encoder(nn.Module):
--- src/models/components/unet_multi_decoder.py
+++ src/models/components/unet_multi_decoder.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from src.models.components.basic_module import ConvBlock, MLP, LinearAttention
+from src.models.components.basic_module import ConvBlock, MLP, LinearAttention, Upsample2D
 from src.models.components.mobile_vit import MobileVit
 
 class UNetMultiDecoderModel(nn.Module):
@@ -17,8 +17,11 @@ class UNetMultiDecoderModel(nn.Module):
         model_channels: int = 32, 
         num_conv_blocks: Union[int, Sequence[int]] = 1,
         channel_mult: Sequence[int] = (1, 1, 1, 1),
+        num_groups: int = 16,
+        use_batch_norm: bool = False,
         use_self_attention: bool = False,
         use_ViT_bottleneck: bool = False,
+        use_interpolation: bool = False,
         use_MLP_out: bool = False,
         use_fp16: bool = False,
     ) -> None:
@@ -28,11 +31,18 @@ class UNetMultiDecoderModel(nn.Module):
         :param out_channels: Channels in the multi output Tensors. Default to `(3, 3, 1)`.
         :param model_channels: Base channel count for the model.
         :param num_conv_blocks: Number of conv blocks(residual blocks) per downsample.
-        :param channel_mult: Channel multiplier for each level of the UNet. Default to `(1, 1, 1, 1)`.
-        :param use_self_attention: Whether to add linear self attention after conv blocks. Default to `True`.
-        :param use_ViT_bottleneck: Whether to use MobileViT at bottleneck. Default to `True`.
-        :param use_MLP_out: Whether to use MLP as the output layer. Default to `True`.
-        :param use_fp16: Whether to use fp16 with mixed precision (not implemented). Default to `False`.
+        :param channel_mult: Channel multiplier for each level of the UNet. Default to 
+                             `(1, 1, 1, 1)`.
+        :param num_group: The number of normalization group. Default to `16`.
+        :param use_batch_norm: Whether to use Batch Normalization. Default to `False`.
+        :param use_self_attention: Whether to add linear self attention after conv blocks. 
+                                   Default to `False`.
+        :param use_ViT_bottleneck: Whether to use MobileViT at bottleneck. Default to `False`.
+        :param use_interpolation: Whether to use interpolation for upsampling in decoder. 
+                                  Default to `False`.
+        :param use_MLP_out: Whether to use MLP as the output layer. Default to `False`.
+        :param use_fp16: Whether to use fp16 with mixed precision (not implemented). 
+                         Default to `False`.
         """
         super().__init__()
         
@@ -57,7 +67,10 @@ class UNetMultiDecoderModel(nn.Module):
         # downsampling
         layers = []
         for ch in fea_channels:
-            layers.append(ConvBlock(in_channels, ch))
+            layers.append(ConvBlock(in_channels=in_channels, 
+                                    out_channels=ch, 
+                                    use_batch_norm=use_batch_norm, 
+                                    num_groups=num_groups))
             if use_self_attention:
                 layers.append(LinearAttention(dim=ch))
             self.input_blocks.append(nn.Sequential(*layers))
@@ -69,21 +82,33 @@ class UNetMultiDecoderModel(nn.Module):
         if use_ViT_bottleneck:
             self.middle_block = MobileVit()
         else:
-            self.middle_block = ConvBlock(in_channels, in_channels)
+            self.middle_block = ConvBlock(in_channels=in_channels, 
+                                          out_channels=in_channels, 
+                                          use_batch_norm=use_batch_norm, 
+                                          num_groups=num_groups)
 
         # multi decoders
         for out_ch in out_channels:
             # out module of Model
             if use_MLP_out:
-                self.outs.append(MLP(in_channels=fea_channels[0] // 2, out_channels=out_ch))
+                self.outs.append(MLP(in_channels=fea_channels[0] // 2, 
+                                     out_channels=out_ch, 
+                                     use_batch_norm=use_batch_norm, 
+                                     num_groups=num_groups))
             else:
                 self.outs.append(nn.Conv2d(in_channels=fea_channels[0] // 2, out_channels=out_ch, kernel_size=1))
             
             # upsampling
             self.output_blocks.append(nn.ModuleList())
             for ch in reversed(fea_channels):
-                layers.append(nn.ConvTranspose2d(in_channels=ch, out_channels=ch // 2, kernel_size=2, stride=2))
-                layers.append(ConvBlock(in_channels=ch, out_channels=ch // 2))
+                if use_interpolation:
+                    layers.append(Upsample2D(in_channels=ch, out_channels= ch // 2, mode="nearest"))
+                else:
+                    layers.append(nn.ConvTranspose2d(in_channels=ch, out_channels=ch // 2, kernel_size=2, stride=2))
+                layers.append(ConvBlock(in_channels=ch, 
+                                        out_channels=ch // 2, 
+                                        use_batch_norm=use_batch_norm, 
+                                        num_groups=num_groups))
                 self.output_blocks[-1].append(nn.ModuleList(layers)) 
                 layers.clear()
         
@@ -112,8 +137,8 @@ class UNetMultiDecoderModel(nn.Module):
                 outs[idx] = modules[1](torch.cat([modules[0](outs[idx]), hs[level]], dim=1))
                 
             outs[idx] = self.outs[idx](outs[idx]).type(x.dtype)
-            
-        return torch.tanh(torch.concat(outs, dim=1))
+            outs[idx] = torch.tanh(outs[idx])
+        return torch.concat(outs, dim=1)
     
     
 if __name__ == "__main__":
@@ -122,6 +147,7 @@ if __name__ == "__main__":
                                 channel_mult=(1, 1, 1, 1),
                                 use_self_attention=False,
                                 use_ViT_bottleneck=False,
+                                use_interpolation=True,
                                 use_MLP_out=False,
                                 use_fp16=False)
     print(model)
