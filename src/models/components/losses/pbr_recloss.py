568201980e2e5f50cf56ce43e35c2a72357ff707
Lingstreasure <1486520814@qq.com>
2023-08-10 15:09:19 +0800

    1.Add rec loss weights for each pbr map. 2.Add alex option in args. 3.Add epoch condition for using per loss. 4.Add channel numbers judgement in forward().

--- src/models/components/losses/pbr_recloss.py
+++ src/models/components/losses/pbr_recloss.py
@@ -1,4 +1,5 @@
-from typing import Dict, Tuple
+from contextlib import contextmanager
+from typing import Any, Dict, List, Literal, Tuple
 
 import torch
 from torch import nn
@@ -8,79 +9,118 @@ from src.models.components.losses.lpips import LPIPS
 
 class PBRReconstructionLoss(nn.Module):
     """The PBR maps reconstruction losses module.
-    
-    The loss module contain reconstruction loss, perceptual loss, render loss and GAN-based losses. 
+
+    The loss module contain reconstruction loss, perceptual loss, render loss and GAN-based losses.
     """
-    
+
     def __init__(
         self,
-        random_per_loss_weight: float = 0., 
-        per_loss_weight: float = 0., 
-        render_loss_weight: float = 0., 
-        gan_loss_weight: float = 0.
+        rec_loss_weights: List[float],
+        pbr_ch_nums: List[int],
+        per_loss_net: Literal["alex", "vgg"] = "alex",
+        per_loss_after_num_epochs: int = 10,
+        random_per_loss_weight: float = 0.0,
+        per_loss_weight: float = 0.0,
+        render_loss_weight: float = 0.0,
+        gan_loss_weight: float = 0.0,
     ) -> None:
         """
+        :param rec_loss_weights: The list of reconstruction loss weights between pbr maps, equal with numbers of model decoders.
+        :param pbr_ch_nums: The list contained channel numbers of multi pbr maps, equal with numbers of model decoders.
+        :param per_loss_net: The pretrained net used for calculating LPIPS. Default to `alex`.
+        :param per_loss_after_num_epochs: Activate the perceptual loss after the number epochs. Default to `10`.
         :param random_per_loss_weight: The weight of channels-random-selected perceptual loss. Default to `0.`.
         :param per_loss_weight: The weight of perceptual loss. Default to `0.`.
         :param render_loss_weight: The weight of differentialable rendering loss. Default to `0.`.
         :param gan_loss_weight: The weight of GAN-based loss. Default to `0.`.
         """
         super().__init__()
-        
-        if random_per_loss_weight > 0. or per_loss_weight > 0.:
-            self.perceptual_loss = LPIPS().eval()
-            ## using different layer weights of VGG19
-            # layer_weights={"conv1_2": 0.1, "conv2_2": 0.1, "conv3_4": 1., "conv4_4": 1., "conv5_4": 1.}
-            # self.perceptual_loss = basicsr.losses.basic_loss.PerceptualLoss(layer_weights=layer_weights)
-            
-        if render_loss_weight > 0.:
+        assert len(rec_loss_weights) == len(
+            pbr_ch_nums
+        ), "rec loss weights list and should own same length with pbr map numbers"
+
+        self.rec_loss_weights = rec_loss_weights
+        self.pbr_ch_nums = pbr_ch_nums
+
+        if random_per_loss_weight > 0.0 or per_loss_weight > 0.0:
+            self.perceptual_loss = LPIPS(net=per_loss_net).eval()
+
+        if render_loss_weight > 0.0:
             # self.renderer = Renderer(normal_format='dx')
             pass
-            
+
+        self.p_loss_after_num_epochs = per_loss_after_num_epochs
         self.random_per_loss_weight = random_per_loss_weight
         self.per_loss_weight = per_loss_weight
         self.render_loss_weight = render_loss_weight
         self.gan_loss_weight = gan_loss_weight
 
-    def forward(self, preds: torch.Tensor, gts: torch.Tensor) -> Tuple[float, Dict[str, torch.Tensor]]:
+    def forward(
+        self, preds: torch.Tensor, targets: torch.Tensor, cur_epoch: int
+    ) -> Tuple[float, Dict[str, torch.Tensor]]:
+        """Cal the losses between preds and targets."""
         B, C, H, W = preds.shape
-        
-        # pixel-wised reconstruction loss - the base loss
-        rec_loss = torch.abs(preds.contiguous() - gts.contiguous()).mean()
-        
-        # logger dict for each loss
-        log = {"rec_loss": rec_loss.detach()}
-        
-        # perceptual loss
-        p_loss = 0.
-        if self.per_loss_weight > 0.:
-            p_loss += self.perceptual_loss(preds[:, :3, ...].contiguous(), gts[:, :3, ...].contiguous())[0].mean()
-            p_loss += self.perceptual_loss(preds[:, 3:6, ...].contiguous(), gts[:, 3:6, ...].contiguous())[0].mean()
-            p_loss += self.perceptual_loss(preds[:, 6:7, ...].repeat([1, 3, 1, 1]).contiguous(), gts[:, 6:7, ...].repeat([1, 3, 1, 1]).contiguous())[0].mean()
-            p_loss += self.perceptual_loss(preds[:, 7:8, ...].repeat([1, 3, 1, 1]).contiguous(), gts[:, 7:8, ...].repeat([1, 3, 1, 1]).contiguous()).mean()
+        log = {}  # logger dict for each loss
+
+        # pixel-wised reconstruction loss, calculate each map loss separately - the base loss
+        rec_loss = 0.0
+        start_idx = 0
+        for idx, num in enumerate(self.pbr_ch_nums):
+            end_idx = start_idx + num
+            single_map_loss = torch.abs(
+                preds[:, start_idx:end_idx, ...].contiguous()
+                - targets[:, start_idx:end_idx, ...].contiguous()
+            ).mean()
+            rec_loss += single_map_loss * self.rec_loss_weights[idx]
+            log[f"rec_loss/{idx}"] = single_map_loss.detach()
+            start_idx = end_idx
+
+        log["rec_loss"] = rec_loss.detach()
+
+        # perceptual loss, only start when weight and epoch conditions are met
+        p_loss = 0.0
+        if self.per_loss_weight > 0.0 and cur_epoch > self.p_loss_after_num_epochs:
+            start_idx = 0
+            for num in self.pbr_ch_nums:
+                end_idx = start_idx + num
+                p_loss += self.perceptual_loss(
+                    preds[:, start_idx:end_idx, ...].contiguous(),
+                    targets[:, start_idx:end_idx, ...].contiguous(),
+                ).mean()
+                start_idx = end_idx
+
             p_loss *= self.per_loss_weight
             log["per_loss"] = p_loss.detach()
-        
-        # differentiable rendering loss
-        render_loss = 0.
-        if self.render_loss_weight > 0.:
-            if self.renderer.device != gts.device:
-                self.renderer.to(gts.device)
-            render_imgs = self.renderer.evaluate(*[(preds[:, :3, ...] + 1.0) / 2.0, 
-                                                   (preds[:, 3:6, ...] + 1.0) / 2.0, 
-                                                   (preds[:, 6:7, ...] + 1.0) / 2.0, 
-                                                   (preds[:, 7:, ...] + 1.0) / 2.0, 
-                                                   torch.ones((B, 1, H, W), device=gts.device).float()])
-            render_loss = torch.abs((preds * 2.0 - 1).contiguous() - render_imgs.contiguous()).mean() * self.render_weight
-            log["render_loss"] = render_loss.detach()
 
-        # channels-random-selected perceptual loss
-        random_per_loss = 0.
-        if self.random_per_loss_weight > 0.:
+        # channels-random-selected perceptual loss, same conditions as above
+        random_per_loss = 0.0
+        if self.random_per_loss_weight > 0.0 and cur_epoch > self.p_loss_after_num_epochs:
             indexes = torch.randint(0, C, (3,))  # random select 3 channels
-            random_per_loss = self.perceptual_loss(preds[:, indexes, ...].contiguous(), 
-                                                 gts[:, indexes, ...].contiguous()).mean() * self.random_per_loss_weight
+            random_per_loss = self.perceptual_loss(
+                preds[:, indexes, ...].contiguous(), targets[:, indexes, ...].contiguous()
+            ).mean()
+            random_per_loss = random_per_loss * self.random_per_loss_weight
             log["random_per_loss"] = random_per_loss.detach()
+
+        # differentiable rendering loss
+        render_loss = 0.0
+        if self.render_loss_weight > 0.0:
+            if self.renderer.device != targets.device:
+                self.renderer.to(targets.device)
+            render_imgs = self.renderer.evaluate(
+                *[
+                    (preds[:, :3, ...] + 1.0) / 2.0,
+                    (preds[:, 3:6, ...] + 1.0) / 2.0,
+                    (preds[:, 6:7, ...] + 1.0) / 2.0,
+                    (preds[:, 7:, ...] + 1.0) / 2.0,
+                    torch.ones((B, 1, H, W), device=targets.device).float(),
+                ]
+            )
+            render_loss = (
+                torch.abs((preds * 2.0 - 1).contiguous() - render_imgs.contiguous()).mean()
+                * self.render_weight
+            )
+            log["render_loss"] = render_loss.detach()
 
         # total loss
         loss = rec_loss + random_per_loss + render_loss + p_loss
