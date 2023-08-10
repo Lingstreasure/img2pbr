2c0e102775e6f4bd5a583d573569489652d010aa
Lingstreasure <1486520814@qq.com>
2023-08-10 15:39:40 +0800

    Remove the metalness.

--- src/data/components/pbr_dataset.py
+++ src/data/components/pbr_dataset.py
@@ -1,18 +1,20 @@
 import os
 from typing import Any, List, Tuple
 
-import numpy as np
 import hydra
+import numpy as np
+from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from PIL import Image
 
 
 class PBROvercastHdrRenderDataset(Dataset):
+    """PBR maps dataset with overcast-HDR-rendering images."""
+
     def __init__(
         self,
         data_root_dir: str,
-        data_list_file_dir: str, 
+        data_list_file_dir: str,
         subdataset_names: list,
         pbr_dir_names: list,
         rendered_dir_names: list,
@@ -20,7 +22,7 @@ class PBROvercastHdrRenderDataset(Dataset):
         pbr_maps_type: str = "all",
         img_transforms: list = [],
         debug_on: bool = False,
-        debug_data_num: int = 100, 
+        debug_data_num: int = 100,
     ) -> None:
         """Initialize a `PBROvercastHdrRenderDataset`.
 
@@ -47,26 +49,28 @@ class PBROvercastHdrRenderDataset(Dataset):
         self.debug_data_num = debug_data_num if debug_on else 1000000000
         self._tform = self._set_transforms(img_transforms)
         self._data = self._preprocess()
-        
+
     def __len__(self) -> int:
         """Get the number of samples in dataset.
+
         :return: The number of samples in dataset.
         """
         return len(self._data)
-        
+
     def _preprocess(self) -> List[List[Any]]:
         """Process the raw data and match different pbr maps togather.
+
         :return: The list of processed samples.
         """
         # list to be returned
         data = []
-        
+
         # load data of different subdatasets with specific data_list_file
         for dataset_name, pbr_dir_name, rendered_dir_name in zip(
             self.subdataset_names, self.pbr_dir_names, self.rendered_dir_names
         ):
             data_list_file_path = os.path.join(
-                self.data_list_file_dir, dataset_name + '_' + self.mode + '.txt'
+                self.data_list_file_dir, dataset_name + "_" + self.mode + ".txt"
             )
             # sample name list
             sample_names = []
@@ -80,97 +84,113 @@ class PBROvercastHdrRenderDataset(Dataset):
             cnt = 0
             # load all pbr maps and rendered imgs with sample name list
             for name in sample_names:
-                # debug with a subset of whole datatset
+                # debug with a subset of whole dataset
                 if len(data) >= self.debug_data_num:
                     break
-                
+
                 rendered_path = os.path.join(
                     self.data_root_dir, rendered_dir_name, name + "_512.png"
                 )
                 if not os.path.isfile(rendered_path):
                     continue
-                
+
                 gt_paths = {}
                 pbr_sample_dir = os.path.join(self.data_root_dir, pbr_dir_name, name)
-                gt_paths['albedo'] = os.path.join(pbr_sample_dir, 'color_512.jpg')
-                gt_paths['normal'] = os.path.join(pbr_sample_dir, 'normaldx_512.jpg')
-                gt_paths['roughness'] = os.path.join(pbr_sample_dir, 'roughness_512.jpg')
-                gt_paths['metalness'] = os.path.join(pbr_sample_dir, 'metallic_512.jpg')
-                
+                gt_paths["albedo"] = os.path.join(pbr_sample_dir, "color_512.jpg")
+                gt_paths["normal"] = os.path.join(pbr_sample_dir, "normaldx_512.jpg")
+                gt_paths["roughness"] = os.path.join(pbr_sample_dir, "roughness_512.jpg")
+                # gt_paths['metalness'] = os.path.join(pbr_sample_dir, 'metallic_512.jpg')
+
                 isValid = True
                 # check each pbr map path except metal.
                 for key, path in gt_paths.items():
-                    if key == 'metalness':
+                    if key == "metalness":
                         continue
                     elif not os.path.isfile(path):
                         isValid = False
                         break
                 if not isValid:
                     continue
-                
+
                 # load imgs
                 input = Image.open(rendered_path)  # PIL.Image，h w 3
                 input = np.asarray(input.convert("RGB"))
-                
+
                 gts = {}
-                color = Image.open(gt_paths['albedo'])  # h w 3
+                color = Image.open(gt_paths["albedo"])  # h w 3
                 color = np.asarray(color.convert("RGB"))
-                gts['albedo'] = color
+                gts["albedo"] = color
 
-                normal = Image.open(gt_paths['normal'])  # h w 3
+                normal = Image.open(gt_paths["normal"])  # h w 3
                 normal = np.asarray(normal.convert("RGB"))
-                gts['normal'] = normal
-                
-                roughness = Image.open(gt_paths['roughness'])  # h w 3
-                roughness = np.asarray(roughness.convert("RGB"))
-                gts['roughness'] = roughness[..., 0, None]  # h w 3 -> h w 1
-                
-                metalness = np.zeros((512, 512, 1)).astype(np.uint8)
-                if os.path.exists(gt_paths['metalness']):
-                    metalness = Image.open(gt_paths['metalness'])
-                    metalness = np.asarray(metalness.convert("RGB"))
-                gts['metalness'] = metalness[..., 0, None]  # h w 3 -> h w 1
+                gts["normal"] = normal
 
-                gt = np.concatenate([gts['albedo'], gts['normal'], gts["roughness"], gts["metalness"]], axis=-1)
+                roughness = Image.open(gt_paths["roughness"])  # h w 3
+                roughness = np.asarray(roughness.convert("RGB"))
+                gts["roughness"] = roughness[..., 0, None]  # h w 3 -> h w 1
+
+                # metalness = np.zeros((512, 512, 1)).astype(np.uint8)
+                # if os.path.exists(gt_paths['metalness']):
+                #     metalness = Image.open(gt_paths['metalness'])
+                #     metalness = np.asarray(metalness.convert("RGB"))
+                # gts['metalness'] = metalness[..., 0, None]  # h w 3 -> h w 1
+
+                # gt = np.concatenate([gts['albedo'], gts['normal'], gts["roughness"], gts["metalness"]], axis=-1)
+                gt = np.concatenate([gts["albedo"], gts["normal"], gts["roughness"]], axis=-1)
                 data.append([name, input, gt])
                 cnt += 1
 
         return data
-    
+
     def _set_transforms(self, img_transforms: list = [transforms, ...]) -> transforms:
-        """Set the transforms, such as resize(), ToTensor(), for imgs. 
-        :return: The composed img transforms in torchvision.tranforms.
+        """Set the transforms, such as resize(), ToTensor(), for imgs.
+
+        :return: The composed img transforms in torchvision.transforms.
         """
         img_transforms = [hydra.utils.instantiate(tt) for tt in img_transforms]
-        img_transforms.extend([
-            transforms.ToTensor(),  # row_data -> (0, 1)
-            transforms.Lambda(lambda x: x * 2. - 1.)  # (0, 1) -> (-1, 1)
-        ])
+        img_transforms.extend(
+            [
+                transforms.ToTensor(),  # row_data -> (0, 1)
+                transforms.Lambda(lambda x: x * 2.0 - 1.0),  # (0, 1) -> (-1, 1)
+            ]
+        )
         return transforms.Compose(img_transforms)
-    
+
     def _img_process(self, image: Image) -> Image:
         """Apply the transforms to imgs before entering the model.
+
         :return: The transformed imgs.
         """
         return self._tform(image)
-    
+
     def __getitem__(self, index) -> Tuple[Any]:
+        """Get one item of dataset."""
         name, input, gt = self._data[index]
         input = self._img_process(input)
         gt = self._img_process(gt)
         return (name, input, gt)
-    
+
 
 if __name__ == "__main__":
     dataset = PBROvercastHdrRenderDataset(
         data_root_dir="/media/d5/7D1922F98D178B12/dw/mat_reorg",
-        data_list_file_dir="/home/d5/hz/Code/sdmat/data_file", 
-        subdataset_names=['3dtextures', 'ambient', 'polyhaven', 'sharetextures'],
-        pbr_dir_names=['3dtextures_cleanup_nometal', 'ambient_cleanup_nometal', 'polyhaven_cleanup', 'sharetextures_cleanup_nometal'],
-        rendered_dir_names=['3dtextures_renderout_blender', 'ambient_renderout_blender', 'polyhaven_renderout_blender', 'sharetextures_renderout_blender'],
-        mode='train',
-        pbr_maps_type='all',
+        data_list_file_dir="/home/d5/hz/Code/sdmat/data_file",
+        subdataset_names=["3dtextures", "ambient", "polyhaven", "sharetextures"],
+        pbr_dir_names=[
+            "3dtextures_cleanup_nometal",
+            "ambient_cleanup_nometal",
+            "polyhaven_cleanup",
+            "sharetextures_cleanup_nometal",
+        ],
+        rendered_dir_names=[
+            "3dtextures_renderout_blender",
+            "ambient_renderout_blender",
+            "polyhaven_renderout_blender",
+            "sharetextures_renderout_blender",
+        ],
+        mode="train",
+        pbr_maps_type="all",
         img_transforms=[],
-        debug_on=True
+        debug_on=True,
     )
     print(len(dataset))
